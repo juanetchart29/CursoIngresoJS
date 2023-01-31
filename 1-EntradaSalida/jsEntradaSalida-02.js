b26b137aea32e923ddd9f442f9c092731dcd328a
juanetchart29 <114407900+juanetchart29@users.noreply.github.com>
2023-01-31 12:37:39 -0300

    ejercicio 2 + concatenar

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -2,13 +2,23 @@
 Juan Pablo Etchart
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+
 function mostrar()
 {
 	// las variables en javascript se pueden empezar con let o var
 	// en este curso vamos a usar let
 	let nombrePersona=prompt("Ingrese su Nombre ");
 	
-	alert(nombrePersona);
+	alert(nombrePersona);   
 }
 
-
+/*
+//concatenar dos variables 
+function mostrar(){
+	let nombrePersona
+	nombrePersona=prompt("ingrese su nombre")
+	let apellidoPersona
+	apellidoPersona=prompt("ingrese su apellido")
+	alert("el nombre completo es: "+ nombrePersona +" "+ apellidoPersona)
+}
+*/
