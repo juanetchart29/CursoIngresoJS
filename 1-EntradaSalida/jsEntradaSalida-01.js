27b7abd49262d07c28462b619bf01e97c9942b9b
juanetchart29 <114407900+juanetchart29@users.noreply.github.com>
2023-01-31 09:42:30 -0300

    modifique un ejercicio 1

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,11 @@
 /*
 Al presionar el  botón, 
-se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+se debe mostrar un mensaje como el siguiente 
+"Esto funciona de maravilla"
+*/ 
+
 function mostrar()
 {
-	alert("esto funciona de maravilla");
+	alert("Esto funciona de maravilla");
 }
 
