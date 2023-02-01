db21f287623771e239a4ef0a31cfccfcd289db12
juanetchart29 <114407900+juanetchart29@users.noreply.github.com>
2023-02-01 17:41:22 -0300

    ejercicio 3 y 3biss

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,12 +1,35 @@
 /*
+nombre:Juan Pablo Etchart
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	let nombreIngresado;
+
+	nombreIngresado=document.getElementById("txtIdNombre").value;
+
+	alert(nombreIngresado);
 
 }
 
+/*
+Nombre: Juan Pablo Etchart
+Ejercicio 3.bis: Ingresar descripción (por prompt)
+ y precio de un producto (por id).
+El programa deberá mostrar por alert
+ la descripción del producto 
+junto al precio aumentado en un 30%. 
+Pueden utilizar el html del ejercicio 3 para resolverlo
+*/
 
+function mostrar(){
+	let descripcion;
+	let precio;
+	let precio30;
+
+	descripcion=prompt("ingrese la descripcion del producto ");
+	precio=parseInt(document.getElementById("txtIdNombre").value);
+	precio30= precio*(1.3);
+	alert("el precio de "+descripcion+" es: "+precio30);
+}
+*/
