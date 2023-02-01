a145c20bcd792e5b0412a1a5513a47240739af8c
juanetchart29 <114407900+juanetchart29@users.noreply.github.com>
2023-02-01 12:46:33 -0300

    ejercicio 4 listo para entragar

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,14 @@
 /* 
+Nombre: Juan Pablo Etchart
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	let valor;
+
+	valor=prompt("ingrese el valor");
+
+	document.getElementById("txtIdNombre").value=valor;
 }
 
