899c544d67622648710f78c9917125ace7e4bc26
juanetchart29 <114407900+juanetchart29@users.noreply.github.com>
2023-02-02 09:00:44 -0300

    #

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -4,6 +4,7 @@ Debemos lograr tomar el importe por ID ,
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
+//para cambiar porcentaje linea 14
 function mostrarAumento()
 {
 	let num;
