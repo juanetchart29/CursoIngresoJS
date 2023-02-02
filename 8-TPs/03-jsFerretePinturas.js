666ce2635ed55eac46ca1b431bf6fbd2873a767a
juanetchart29 <114407900+juanetchart29@users.noreply.github.com>
2023-02-01 22:37:38 -0300

    tp3 listo para entregar

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -1,13 +1,28 @@
-/*3.	Para el departamento de Pinturas:
+/*3.
+Nombre:Juan Pablo Etchart
+Para el departamento de Pinturas:
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
 function FahrenheitCentigrados () 
 {
-	
+	let fahr;
+    let cent;
+
+    fahr=parseInt(document.getElementById("txtIdTemperatura").value);
+    cent=(fahr-32)*5/9;
+
+    alert(fahr+" fahrenheit son "+cent+" centigrados")
 }
 
 function CentigradosFahrenheit () 
 {
-	
+	let fahr;
+    let cent;
+    
+    cent=parseInt(document.getElementById("txtIdTemperatura").value);
+    fahr=(cent*9/5)+32;
+
+    alert(cent+" centigrados son "+fahr+" fahrenheit" )
+
 }
