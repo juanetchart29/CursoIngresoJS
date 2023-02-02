c03f0b8adfcb0a59feaaf41baff7aaa24ee806e2
juanetchart29 <114407900+juanetchart29@users.noreply.github.com>
2023-02-01 23:03:16 -0300

    elercicio 5 bis listo para entregar

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -2,8 +2,8 @@
 Nombre:Juan Pablo Etchart 
 Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
-function mostrar()
-{	
+/*function mostrar()
+ {	
 	let nombre;
 	let edad;
 	let mensaje
@@ -13,5 +13,23 @@ function mostrar()
 	mensaje= "el nombre es = "+(nombre)+" y tiene "+(edad)+" años";
 
 	alert(mensaje);
-}
+} */
+// Ejercicio 5.bis: 
+/* se debe mostrar un mensaje :
+ "Perez , usted se llama jose y tiene 66 años"
+se debe optener el apellido , de la manera que puedan */
+function mostrar()
+{	
+	let nombre;
+	let edad;
+	let mensaje;
+	let apellido;
 
+	nombre=document.getElementById("txtIdNombre").value;
+	edad=document.getElementById("txtIdEdad").value;
+	apellido=prompt("ingrese su apellido: ")
+
+	mensaje= apellido+" el nombre es: "+(nombre)+" y tiene "+(edad)+" años";
+
+	alert(mensaje);
+}
