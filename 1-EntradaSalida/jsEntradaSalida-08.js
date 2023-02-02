d121ebd4e579c39dea232bb7456f4cb0e6c74fc5
juanetchart29 <114407900+juanetchart29@users.noreply.github.com>
2023-02-02 08:43:00 -0300

    #

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -1,4 +1,5 @@
 /*
+nombre: Juan Pablo Etchart
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
@@ -15,7 +16,16 @@ function SacarResto()
 
 	alert(resto);
 }*/
-function SacarResto(){
+
+
+
+/* Ejercicio 8.bis: Ingresar dos numeros por id
+Se pide:
+mostrar por alert:
+a) La suma de los dos numeros
+b) El promedio de los numeros
+c) El modulo de los numeros (el primero en modulo del segundo)
+ */function SacarResto(){
 	let n1;
 	let n2;
 	let suma;
