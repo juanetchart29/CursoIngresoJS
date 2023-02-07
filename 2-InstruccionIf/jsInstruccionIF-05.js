9c4596934060f8160826e3923a94dc2c58562cfd
juanetchart29 <114407900+juanetchart29@users.noreply.github.com>
2023-02-07 10:27:48 -0300

    listo para entregar

--- 2-InstruccionIf/jsInstruccionIF-05.js
+++ 2-InstruccionIf/jsInstruccionIF-05.js
@@ -1,5 +1,21 @@
 //nombre: Juan Pablo Etchart
+//manera 1
 function mostrar()
+{
+	let edad;
+	edad=parseInt(document.getElementById("txtIdEdad").value);
+	if(edad<13 || edad>17){
+	
+		alert("la persona no es un adolescente");
+	}
+	 
+		
+
+}//FIN DE LA FUNCIÓN
+
+//manera 2
+/*
+ function mostrar()
 {
 	let edad;
 	edad=parseInt(document.getElementById("txtIdEdad").value);
@@ -10,4 +26,4 @@ function mostrar()
 	 
 		
 
-}//FIN DE LA FUNCIÓN
+} */
