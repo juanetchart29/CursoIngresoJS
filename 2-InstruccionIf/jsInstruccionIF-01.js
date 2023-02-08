c517c4bc644bb5ef14bc75175524f76d0f5c2629
juanetchart29 <114407900+juanetchart29@users.noreply.github.com>
2023-02-08 11:49:02 -0300

    terminados

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,5 +1,5 @@
 //nombre: Juan Pablo Etchart
-function mostrar()
+/* function mostrar()
 {
 	//declaro variables
 	let ingresoInt;
@@ -21,10 +21,107 @@ function mostrar()
 	//muestro el mensaje
 	alert(mensaje);
 
+} */
+/* 
+//------------
+Ejercicio 1bis: 
+/con if
+pedir a una persona que fue de vaciones , 
+la distancia que viajo y el tiempo que tardo, para calcular la velocidad
+e informar:
+60 km/hr = muy lento 
+hata 80km/h= lento 
+hasta 100= buen ritmo
+hasta 120= ahi de la ley 
+mas = eso no se hace
+*/
+/* function mostrar()
+{
+	let distancia;
+	let tiempo; 
+	let velocidad;
+	let mensaje;
+
+	distancia=parseFloat(document.getElementById("txtIdEdad").value);// suponiendo que la ingresa en kilometros 
+	tiempo=parseFloat(prompt("Ingrese el tiempo que tardo en recorrer la distancia(en horas)"));
+	velocidad=Math.round(distancia/tiempo);
+	
+	if (velocidad <= 60)
+	{
+		mensaje=" muy lento";
+	}else if(velocidad<=80)
+	{
+		mensaje=" lento";
+	}else if(velocidad<=100)
+	{
+		mensaje=" buen ritmo";
+	}else if (velocidad<=120)
+	{
+		mensaje=" hasta ahi de la ley";
+	}else if(velocidad >120)
+	{
+		mensaje=" eso no se hace";
+	}
+	alert("la velocidad es "+velocidad+"km/h"+mensaje);
+} */
+//------------------
+/* Ejercicio 1 bis bis:
+con if
+ingresar el nombre y los datos requeridos para calcular el IMC , 
+e informar a la persona si es:
+Bajo peso <18.5
+Peso normal 18,5-24.9
+Preobesidad 25-26.9
+Obesidad I 27-29.9
+Obesidad II 30-34.9
+Obesidad III >40 */
+function mostrar()
+{
+	let peso;
+	let altura;
+	let imc;
+	let mensaje;
+
+	peso=parseFloat(document.getElementById("txtIdEdad").value);
+	altura=parseFloat(prompt("Ingrese la altura en metros: "));
+	imc=(peso/(altura*altura).toFixed(1));//lo busque en google para redondear un digito despues de la coma
+	
+
+	if(imc<18.5)
+	{
+		mensaje="bajo peso";
+	}else if(imc<=24.9)
+	{
+		mensaje=" peso normal";
+	}else if(imc<=26.9)
+	{
+		mensaje=" preobesidad";
+	}else if(imc<=29.9)
+	{
+		mensaje=" obesidad I";
+	}else if(imc<=34.9)
+	{
+		mensaje=" obesidad II";
+	}else
+	{
+		mensaje=" obesidad III";
+	}
+	alert("el imc: "+imc+mensaje)
+
 }
-//FIN DE LA FUNCIÓN
-//anotaciones en clase 
+
+
+
+
+
+
+
+
+
+
 /*
+//anotaciones en clase 
+
 	if(condicion)//verdadero o falso (0) true -> verdadero false -> falso
 	{
 		codigo...;
@@ -40,4 +137,13 @@ function mostrar()
 	operadores logicos
     condicionUno	&&  condicionDos            ||               !
 				y          logica             o logica         negacion(se puede poner adelante de un argumento)
+
+
+math.ceil: redondea al proximo numero entero mas grande
+Math.ceil(.95);    // 1
+Math.ceil(4);      // 4
+Math.ceil(7.004);  // 8
+Math.ceil(-0.95);  // -0
+Math.ceil(-4);     // -4
+Math.ceil(-7.004); // -7
 	*/
--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -1,4 +1,6 @@
-/*4.	Para el departamento de iluminación:
+/*4.
+Nombre: Juan Pablo Etchart
+Para el departamento de iluminación:
 Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
 A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
 B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
@@ -10,5 +12,79 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+ 	let cantidad;
+    let marca;
+    let precio;
+    let precioTotal
+    let precioFinal;
+    let impuesto;
+    let aumento;
+
+    impuesto=10;// impuesto %10
+
+    precio=35;
+
+    cantidad=parseInt(document.getElementById("txtIdCantidad").value);
+    marca=document.getElementById("Marca").value;
+
+    precioTotal=precio*cantidad
+
+    // si la cantidad es mayor igual a 6
+
+    if(cantidad>=6)
+    {
+        precioFinal=precioTotal*0.5;//descuento del 50%
+    }else if(cantidad==5)
+    {
+        if (marca=="ArgentinaLuz")
+        {
+            precioFinal=precioTotal*0.6;
+        }else
+        {
+            precioFinal=precioTotal*0.7;
+        }
+    
+    //si la cantidad es 4
+
+    }else if(cantidad==4)
+    {
+        if(marca=="ArgentinaLuz" || marca=="FelipeLamparas")
+        {
+            precioFinal=precioTotal*0.75;
+        }else
+        {
+            precioFinal=precioTotal*0.8;
+        }
+     
+    //si la cantidad es 3    
+
+    }else if(cantidad==3)
+    {
+        if(marca=="ArgentinaLuz")
+        {
+            precioFinal=precioTotal*0.85;
+        }else if(marca=="FelipeLamparas")
+        {
+            precioFinal=precioTotal*0.9;
+        }else 
+        {
+            preciofinal=precioTotal*0.95;
+        }
+    
+    //si la cantidad es 1 o 2
+
+    }else 
+    {
+        precioFinal=precioTotal
+    }
+    
+    document.getElementById("txtIdprecioDescuento").value=precioFinal;
+
+    //si supera los $120
+    if (precioFinal>120)
+    {
+        aumento=precioFinal*impuesto/100;
+        alert("usted esta pagando: $"+aumento+" de IIBB");
+        precioFinal=precioFinal+aumento;
+    }
 }
