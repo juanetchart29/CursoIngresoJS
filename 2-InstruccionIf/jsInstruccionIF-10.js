723deac34b59a04448686e6ce7ccd53c5ed51690
juanetchart29 <114407900+juanetchart29@users.noreply.github.com>
2023-02-08 09:15:35 -0300

    listo para entregar

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,23 @@
+//nombre: Juan Pablo Etchart
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	let numRandom;
+	let mensaje;
 
+	numRandom=Math.floor(Math.random()*11);
+
+	if (numRandom>=9)
+	{
+		mensaje=" EXCELENTE";
+
+	}else if( numRandom>=4)
+	{
+		mensaje=" APROBO";
+
+	}else
+	{
+		mensaje=" Vamos, la proxima se puede";
+	}
+	alert("la nota es: "+numRandom+mensaje)
 }//FIN DE LA FUNCIÓN
