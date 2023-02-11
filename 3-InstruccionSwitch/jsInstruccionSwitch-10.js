092dd9541d263f4b339a59c2db986aa28b90aa5b
juanetchart29 <114407900+juanetchart29@users.noreply.github.com>
2023-02-11 11:27:50 -0300

    listo sw 10

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,62 @@
+// nombre: Juan Pablo Etchart
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	var estacionIngresada;
+	let destinoIngresado;
+	let mensaje;
 
+	estacionIngresada=document.getElementById("txtIdEstacion").value;
+	destinoIngresado=document.getElementById("txtIdDestino").value;
+
+	switch(estacionIngresada)
+	{
+		case "Invierno":
+			switch(destinoIngresado){
+				case "Bariloche":
+					mensaje="se viaja";
+					break;
+				case "Cataratas":
+				case "Cordoba":
+				case "Mar del plata":
+					mensaje="no se viaja";
+					break;
+			}
+			break;
+		case "Verano":
+			switch(destinoIngresado)
+			{
+				case "Bariloche":
+				case "Cordoba":
+					mensaje="no se viaja";
+					break;
+				case "Cataratas":	
+				case "Mar del plata":
+					mensaje="se viaja";
+					break;
+			}
+			break;
+		case "Primavera":
+			switch(destinoIngresado)
+			{
+				case "Bariloche":
+					mensaje="no se viaja";
+					break;
+				case "Cordoba":
+				case "Cataratas":	
+				case "Mar del plata":
+					mensaje="se viaja";
+					break;
+			
+			}
+			break;
+		case "Otoño":
+			mensaje="se viaja";
+			break;
+	}	
+	alert(mensaje);
+	
+	
+	
+	
+	
 }//FIN DE LA FUNCIÓN
