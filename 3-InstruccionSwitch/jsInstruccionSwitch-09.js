b16b7ccb0d79989e91824b22e0d78d2d507b60b1
juanetchart29 <114407900+juanetchart29@users.noreply.github.com>
2023-02-11 11:27:30 -0300

    listo sw 9

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -6,14 +6,57 @@ function mostrar()
 	let mensaje;
 	let precio;
 
-	precio=15000
-	porcentaje=0
+	precio=15000;
+	porcentaje=0;
 
 	estacionIngresada=document.getElementById("txtIdEstacion").value;
 	destinoIngresado=document.getElementById("txtIdDestino").value;
 
-
-
-	alert(estacionIngresada);
-
+	switch(estacionIngresada)
+	{
+		case "Invierno":
+			switch(destinoIngresado){
+				case "Bariloche":
+					porcentaje=20;
+					break;
+				case "Cataratas":
+				case "Cordoba":
+					porcentaje=-10;
+					break;
+				case "Mar del plata":
+					porcentaje=-20;
+					break;
+			}
+			break;
+		case "Verano":
+			switch(destinoIngresado)
+			{
+				case "Bariloche":
+					porcentaje=-20;
+					break;
+				case "Cataratas":
+				case "Cordoba":
+					porcentaje=10
+					break;
+				case "Mar del plata":
+					porcentaje=20;
+					break;
+			}
+			break;
+		default:
+			switch(destinoIngresado)
+			{
+				case "Cordoba":
+					porcentaje=0;
+					break;
+				default:
+					porcentaje=10;
+					break;
+			
+			}
+			break;
+	}
+	precio=precio+precio*porcentaje/100
+	mensaje="el precio es:  $"+precio;
+	alert(mensaje);
 }//FIN DE LA FUNCIÓN
