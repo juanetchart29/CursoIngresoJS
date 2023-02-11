92ffaf56a3c558b9fb3b4e6a84de54b02c12ace5
juanetchart29 <114407900+juanetchart29@users.noreply.github.com>
2023-02-11 11:27:12 -0300

    listo para entregar tp4

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -8,7 +8,7 @@ C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas
 D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
 E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
-
+ HECHO TODO CON IF
  */
 /* function CalcularPrecio () 
 {
@@ -73,7 +73,7 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
             descuento=5;
         }
     
-    //si la cantidad es 1 o 2
+
     }
     precioFinal=precioTotal*(1-descuento/100)
     //si supera los $120
@@ -87,8 +87,9 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
     document.getElementById("txtIdprecioDescuento").value=precioFinal;
 
     
-} */
-// HECHO CON SWITCH
+} *//*
+----------------------------------------------------------------
+HECHO TODO CON SWITCH
 function CalcularPrecio ()
 {
     let cantidad;
@@ -106,13 +107,14 @@ function CalcularPrecio ()
 
     cantidad=parseInt(document.getElementById("txtIdCantidad").value);
     marca=document.getElementById("Marca").value;
+    precioTotal= cantidad*precio
 
     switch(cantidad)
     {
         case 1:
         case 2:
-            descuento=0
-            break
+            descuento=0;
+            break;
         case 3:
             switch(marca)
             {
@@ -127,6 +129,7 @@ function CalcularPrecio ()
                     descuento=5
                     break;
             }
+            break;
         case 4:
             switch(marca)
             {
@@ -136,9 +139,191 @@ function CalcularPrecio ()
                     break
                 default:
                     descuento=20;
-            }   
-
-
-    }   
+                    break;
+            }
+            break;
+        case 5:
+            switch(marca)
+            {
+                case "ArgentinaLuz":
+                    descuento=40;
+                    break;
+                default:
+                    descuento=30;
+                    break;
+                
+            }
+            break;
+        default:
+            descuento=50
+            break;
+    }
+    precioFinal=precioTotal*(1-descuento/100)
+    //si supera los $120
+    if (precioFinal>120)
+    {
+        aumento=precioFinal*impuesto/100;
+        alert("usted esta pagando: $"+aumento+" de IIBB");
+        precioFinal=precioFinal+aumento;
+    }
+    //muestro el precio final con descuento y aumento(si cumple la condicion)
+    document.getElementById("txtIdprecioDescuento").value=precioFinal;   
 }
-// todo switch, swiutch cantidad y switch marcas
+----------------------------------------------
+// swiutch cantidad 
+function CalcularPrecio ()
+{
+    let cantidad;
+    let marca;
+    let precio;//PRECIO de cada unidad de lampara
+    let precioTotal;//PRECIO X CANTIDAD 
+    let descuento;
+    let precioFinal;//PRECIOTOTAL aplicando su respectivo descuento
+    let impuesto;//IMPUESTO si PRECIOTOTAL supera los 120
+    let aumento;//el valor que se le agrega al PRECIOFINAL por el respectivo impuesto
+
+    impuesto=10;// impuesto %10
+    precio=35;
+    descuento=0;
+
+    cantidad=parseInt(document.getElementById("txtIdCantidad").value);
+    marca=document.getElementById("Marca").value;
+    precioTotal= cantidad*precio
+
+    switch(cantidad)
+    {
+        case 1:
+        case 2:
+            descuento=0;
+            break;
+        case 3:
+            if(marca=="ArgentinaLuz")
+            {
+                descuento=15;
+            }else if(marca=="FelipeLamparas")
+            {
+                descuento=10;
+            }else 
+            {
+                descuento=5;
+            }
+            break;
+        case 4:
+            if(marca=="ArgentinaLuz" || marca=="FelipeLamparas")
+            {
+                descuento=25;
+            }else
+            {
+                descuento=20;
+            }
+            break;
+        case 5:
+                if (marca=="ArgentinaLuz")
+            {
+                descuento= 40;
+            }else
+            {
+                descuento=30;
+            }
+            break;
+        default:
+            descuento=50
+            break;
+    }
+    precioFinal=precioTotal*(1-descuento/100)
+    //si supera los $120
+    if (precioFinal>120)
+    {
+        aumento=precioFinal*impuesto/100;
+        alert("usted esta pagando: $"+aumento+" de IIBB");
+        precioFinal=precioFinal+aumento;
+    }
+    //muestro el precio final con descuento y aumento(si cumple la condicion)
+    document.getElementById("txtIdprecioDescuento").value=precioFinal;  
+}
+*/
+//SWITCH MARCAS
+function CalcularPrecio () 
+{
+ 	let cantidad;
+    let marca;
+    let precio;//PRECIO de cada unidad de lampara
+    let precioTotal;//PRECIO X CANTIDAD 
+    let descuento;
+    let precioFinal;//PRECIOTOTAL aplicando su respectivo descuento
+    let impuesto;//IMPUESTO si PRECIOTOTAL supera los 120
+    let aumento;//el valor que se le agrega al PRECIOFINAL por el respectivo impuesto
+
+    impuesto=10;// impuesto %10
+    precio=35;
+    descuento=0;
+
+    cantidad=parseInt(document.getElementById("txtIdCantidad").value);
+    marca=document.getElementById("Marca").value;
+
+    precioTotal=precio*cantidad
+
+    // si la cantidad es mayor igual a 6
+
+    if(cantidad>=6)
+    {
+        descuento= 50;//descuento del 50%
+
+    }else if(cantidad==5)
+    {
+        switch(marca)
+            {
+                case "ArgentinaLuz":
+                    descuento=40;
+                    break;
+                default:
+                    descuento=30;
+                    break;
+                
+            }
+    
+    //si la cantidad es 4
+
+    }else if(cantidad==4)
+    {
+        switch(marca)
+            {
+                case "ArgentinaLuz":
+                case "FelipeLamparas":
+                    descuento=25;
+                    break
+                default:
+                    descuento=20;
+                    break;
+            }
+    //si la cantidad es 3    
+
+    }else if(cantidad==3)
+    {
+        switch(marca)
+            {
+                case "ArgentinaLuz":
+                    descuento =15
+                    break;
+
+                case "FelipeLamparas":
+                    descuento=10
+                    break;
+                default: 
+                    descuento=5
+                    break;
+            }
+    //si la cantidad es 1 o 2 DESCUENTO = 0 PORQUE YA ESTA DEFINIDO ARRIBA
+    }
+    precioFinal=precioTotal*(1-descuento/100)
+    //si supera los $120
+    if (precioFinal>120)
+    {
+        aumento=precioFinal*impuesto/100;
+        alert("usted esta pagando: $"+aumento+" de IIBB");
+        precioFinal=precioFinal+aumento;
+    }
+    //muestro el precio final con descuento y aumento(si cumple la condicion)
+    document.getElementById("txtIdprecioDescuento").value=precioFinal;
+  
+} 
