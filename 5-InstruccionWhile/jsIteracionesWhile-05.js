3eb86d202b5ae831187944acb0eb52540c4385fd
juanetchart29 <114407900+juanetchart29@users.noreply.github.com>
2023-02-15 08:41:15 -0300

    listo para entregar

--- 5-InstruccionWhile/jsIteracionesWhile-05.js
+++ 5-InstruccionWhile/jsIteracionesWhile-05.js
@@ -1,12 +1,13 @@
 /*
+Nombre: Juan Pablo Etchart
 Al presionar el botón pedir un sexo
 'f' para femenino, 'm' para masculino.*/
 function mostrar()
 {
 	var sexoIngresado;
 	sexoIngresado = prompt("ingrese f ó m .");
-
-
-
+	while(sexoIngresado!="f" && sexoIngresado!="F" && sexoIngresado!="m" && sexoIngresado!="M"){
+		sexoIngresado=prompt("Error: ingrese un genero valido");
+	}
 	txtIdSexo.value=sexoIngresado;
 }//FIN DE LA FUNCIÓN
