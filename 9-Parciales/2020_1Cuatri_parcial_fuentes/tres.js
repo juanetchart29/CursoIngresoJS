dc1ced9d285bdebdc76811d3e3ee96a2360f80f8
juanetchart29 <114407900+juanetchart29@users.noreply.github.com>
2023-02-26 20:05:18 -0300

    3 y 3 bis

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
@@ -1,11 +1,214 @@
+/*  Parcial 2020 3 bis: /* "Mapa Mundi"
+Realizar el algoritmo que permita ingresar 5 paises:
+el continente (validar entre America , Asia , Europa ,Africa y Oceania) 
+el nombre del país, 
+cantidad de habitantes en millones entre 1 y 7000 (validar)
+el nivel de pobresa entre (pobre, rico, millonario) en europa no hay paises pobre(validar)
+la temperaruta mínima que se registra en su territorio(entre -50 y 50)
+a)La cantidad de temperaturas pares.
+b)la cantidad de temperaturas impares de europa
+c)El nombre del pais con menos habitantes
+d)la cantidad de paises que superan los 40 grados.
+e)la cantidad de paises de america que tienen menos de 0 grados .
+f)el promedio de habitantes entre los paises ingresados .
+g)el promedio de habitantes entre los paises que superan los 40 grados
+H) la temperatura mínima ingresada, y nombre del pais que registro esa temperatura. 
+i) que continente tiene mas habitantes.*/ 
+
+
+function mostrar()
+{
+    let continente;
+    let nombre;
+    let cantidadDeHabitantes;
+    let nivlePobresa;
+    let temperatura;
+
+    let contadorTemperaturaPar=0;
+    let contadorTemperaturaImparEuropa=0;
+    let banderaPaisConMenosHabitantes=true;
+    let menorCantidadDeHabitantes;
+    let nombreMenorCantidadDeHabitantes;
+
+    let contadorSuperanCuarenta=0;
+    let contadorAmericaMenosCero=0;
+
+    let contadorVueltas=0;
+    let promedioCantidadHabitantes;
+    let acumuladorCantidadDeHabitantes=0;
+    let acumuladorCantidadDeHabitantesSuperanCuarenta=0;
+    let promedioSuperanCuarenta;
+
+    let banderaTemperaturaMinima=true;;
+    let temperatutaMinima;
+    let nombrePaisTemperaturaMinima;
+    
+    
+    let acumuladorAmerica=0;
+    let acumuladorAsia=0;
+    let acumuladorEuropa=0;
+    let acumuladorAfrica=0;
+    let acumuladorOceania=0;
+
+    let comparador=0;
+    let continenteMasHabitantes;
+
+
+    for(let i=0;i<5;i++)
+    {
+        //inputs de datos---------------------------
+        continente=prompt("ingrese el continente:America ,  , Europa ,Africa y Oceania");
+        while(continente!="America"&&continente!="Asia"&& continente!="Europa"&& continente!="Africa"&& continente!="Oceania" )
+        {
+            continente=prompt("error,ingrese el continente: , Asia , Europa ,Africa y Oceania");
+        }
+
+        nombre=prompt("ingrese el nombre");
+        while(!isNaN(parseInt(nombre)))
+        {
+            nombre=prompt("error,ingrese un nombre  valido");
+        }
+
+        cantidadDeHabitantes=parseInt(prompt("ingrese un numero de habitantes"));//entero
+        while(isNaN(cantidadDeHabitantes)||cantidadDeHabitantes<1 || cantidadDeHabitantes>7000)
+        {
+            cantidadDeHabitantes=parseInt(prompt("Error,ingrese un numero de habitantes valido"));
+        }//fin del while
+        
+        
+        nivlePobresa=prompt('ingrese el nuvel de pobreza: pobre, rico, millonario');
+        while(nivlePobresa!="pobre" && nivlePobresa!= "rico" && nivlePobresa!= "millonario")
+        {
+            nivlePobresa=prompt('error,ingrese el tipo de producto: "rico" , " pobre" o "millonario"');
+        }//fin del while
+
+        temperatura=parseInt(prompt("ingrese una temperatura"));//entero
+        while(isNaN(temperatura)|| temperatura<-50 || temperatura>50)
+        {
+            temperatura=parseInt(prompt("Error,ingrese una temperatura"));
+        }//fin del while
+        //---------------- logica 
+
+        if(temperatura%2==0)
+        {
+            contadorTemperaturaPar+=1;
+        }
+
+
+        switch(continente)
+        {
+            case "Africa":
+                acumuladorAfrica+=cantidadDeHabitantes;
+                break;
+            case "America":
+                if (temperatura<0)
+                {
+                    contadorAmericaMenosCero+=1;
+                }
+                acumuladorAmerica+=cantidadDeHabitantes;
+                break;
+            case "Europa":
+                if (temperatura%2!=0)
+                {
+                    contadorTemperaturaImparEuropa+=1;
+                }
+                acumuladorEuropa+=1;
+                break;
+
+            case "Oceania":
+                acumuladorOceania+=cantidadDeHabitantes;
+                break;
+            case "Asia":
+                acumuladorAsia+=cantidadDeHabitantes;
+                break;
+        }
+
+
+        if (banderaPaisConMenosHabitantes)
+        {
+            menorCantidadDeHabitantes=cantidadDeHabitantes;
+            nombreMenorCantidadDeHabitantes=nombre;
+            banderaPaisConMenosHabitantes=false;
+        }else if (menorCantidadDeHabitantes>cantidadDeHabitantes)
+        {
+            menorCantidadDeHabitantes=cantidadDeHabitantes;
+            nombreMenorCantidadDeHabitantes=nombre;
+        }
+        
+
+        if (temperatura>40)
+        {
+            contadorSuperanCuarenta+=1;
+            acumuladorCantidadDeHabitantesSuperanCuarenta=+temperatura;
+        }
+        
+
+
+        if(banderaTemperaturaMinima)
+        {
+            temperatutaMinima=temperatura;
+            nombrePaisTemperaturaMinima=nombre;
+            banderaTemperaturaMinima=false;
+        }else if (temperatutaMinima>temperatura)
+        {
+            temperatutaMinima=temperatura;
+            nombrePaisTemperaturaMinima=nombre;
+        }
+
+        
+        if(acumuladorAfrica>comparador)
+        {
+            comparador=acumuladorAfrica;
+            continenteMasHabitantes=continente;
+
+        }else if(acumuladorOceania>comparador)
+        {
+            comparador=acumuladorOceania;
+            continenteMasHabitantes=continente;
+        }else if (acumuladorAmerica>comparador)
+        {
+            comparador=acumuladorAmerica;
+            continenteMasHabitantes=continente;
+        }else if(acumuladorAsia>comparador)
+        {
+            comparador=acumuladorAsia;
+            continenteMasHabitantes=continente;
+        }else if(acumuladorEuropa>comparador)
+        {
+            comparador=acumuladorEuropa;
+            continenteMasHabitantes=continente;
+        }
+
+        contadorVueltas+=1;
+        acumuladorCantidadDeHabitantes+=cantidadDeHabitantes;
+    }//fin del for
+    promedioCantidadHabitantes=acumuladorCantidadDeHabitantes/contadorVueltas;
+    promedioSuperanCuarenta=acumuladorCantidadDeHabitantesSuperanCuarenta/contadorSuperanCuarenta;
+
+    document.write("la cantidad de temperaturas pares"+contadorTemperaturaPar+"<br>");
+    document.write("contador de temperatura impar de europa"+contadorTemperaturaImparEuropa+"<br>");        
+    document.write("rl nombre con nenos habitantes"+nombreMenorCantidadDeHabitantes+"<br>");
+    document.write("la cantidad de paises que superan los 40 grados"+contadorSuperanCuarenta+"<br>"); 
+    document.write("la cantidad de paises de america que tienen menos de 0 grados grados"+contadorAmericaMenosCero+"<br>"); 
+    document.write("el promedio de habitantes entre los paises que superan los 40 grados"+promedioSuperanCuarenta+"<br>"); 
+    document.write("el promedio de habitantes entre los paises ingresados"+promedioCantidadHabitantes+"<br>"); 
+    document.write("la temperatura minima y el nombre del pais que registro esa temperatura"+temperatutaMinima+nombrePaisTemperaturaMinima+"<br>"); 
+    document.write("el continente que mas habitantes tiene es "+continenteMasHabitantes+"<br>"); 
+    
+    
+}
+
+
+
 /*
 Nombre: Juan Pablo Etchart
-En el ingreso a un viaje en avion nos solicitan nombre , edad, sexo("f" o "m") y estado civil("soltero", "casado" o "viudo")y temperatura corporal.
+En el ingreso a un viaje en avion nos solicitan nombre , edad, sexo("f" o "m") 
+y estado civil("soltero", "casado" o "viudo")y temperatura corporal.
 a) El nombre de la persona con mas temperatura.
 b) Cuantos mayores de edad estan viudos
 c) La cantidad de hombres que hay solteros o viudos.
 d) cuantas personas de la tercera edad( mas de 60 años) , tienen mas de 38 de temperatura
-e) El promedio de edad entre los hombres solteros. */
+e) El promedio de edad entre los hombres solteros. 
 function mostrar()
 {
     let nombreIngresado;
@@ -112,3 +315,4 @@ function mostrar()
 
 
 }
+*/
